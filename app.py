88520df78cae0e901ead3a3c3c5c254d964c0a20
Angelica <Angelica137@users.noreply.github.com>
2020-08-02 22:09:17 +0100

    user log in

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 
 
 app = Flask(__name__)                                     # these __x__ means thi particular app will take
@@ -10,8 +11,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))      # its name form the na
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')  # you can changfe these to hard coded string
 
 
+app.config['JWT_SECRET_KEY'] = 'super secret' # ∂chamge this for the love of god
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
 
 
 @app.cli.command('db_create')
@@ -68,7 +72,7 @@ def planets():
     return jsonify(result)
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
     email = request.form['email']
     test = User.query.filter_by(email=email).first()
@@ -83,6 +87,22 @@ def register():
         db.session.commit()
         return jsonify(message='User created successfully'), 201
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+
+    test = User.query.filter_by(email=email, password=password).first()
+    if test:
+          access_token = create_access_token(identity=email)
+          return jsonify(message='log in sucessful', access_token=access_token)
+    else:
+          return jsonify(message='Bad email or password'), 401
 
 # database models
 
